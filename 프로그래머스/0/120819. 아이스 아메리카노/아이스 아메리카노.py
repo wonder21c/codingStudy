49c76b52fa369597788fa79bdc42d82fb38831c4
손정우 <50861700+wonder21c@users.noreply.github.com>
2024-06-08 21:43:52 +0900

    Update 아이스 아메리카노.py

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -3,3 +3,9 @@ def solution(money):
     answer[0] = money // 5500
     answer[1] = money - (5500 * answer[0])
     return answer
+
+
+def solution(money):
+
+    answer = [money // 5500, money % 5500]
+    return answer
