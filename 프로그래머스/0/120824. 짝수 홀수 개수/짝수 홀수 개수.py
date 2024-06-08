a377ffdf4ac5aca245d98d12b6c53af5dcda6b22
손정우 <50861700+wonder21c@users.noreply.github.com>
2024-06-08 21:22:41 +0900

    Update 짝수 홀수 개수.py

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -7,3 +7,12 @@ def solution(num_list):
             answer[1] +=1
         
     return answer
+
+
+```python
+def solution(num_list):
+    answer = [0,0]
+    for n in num_list:
+        answer[n%2]+=1
+    return answer
+```
