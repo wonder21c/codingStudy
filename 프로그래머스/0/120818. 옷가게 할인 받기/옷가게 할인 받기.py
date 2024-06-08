4772a9fc9b19edd04ff2c289aa7120bfc2862d2f
손정우 <50861700+wonder21c@users.noreply.github.com>
2024-06-08 21:38:43 +0900

    Update 옷가게 할인 받기.py

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -10,3 +10,12 @@ def solution(price):
     else:
         answer = price
     return math.floor(answer)
+
+
+
+
+def solution(price):
+    discount_rates = {500000: 0.8, 300000: 0.9, 100000: 0.95, 0: 1}
+    for discount_price, discount_rate in discount_rates.items():
+        if price >= discount_price:
+            return int(price * discount_rate)
