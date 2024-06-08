3eb36320b01caeec811d592ef99582559cc1e1d9
손정우 <50861700+wonder21c@users.noreply.github.com>
2024-06-08 22:26:56 +0900

    Update 가위 바위 보.py

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -9,3 +9,17 @@ def solution(rsp):
             answer += '2'
         
     return answer
+
+
+def solution(rsp):
+    d = {'0':'5','2':'0','5':'2'}
+    return ''.join(d[i] for i in rsp)
+
+def solution(rsp):
+    rsp =rsp.replace('2','s')
+    rsp =rsp.replace('5','p')
+    rsp =rsp.replace('0','r')
+    rsp =rsp.replace('r','5')
+    rsp =rsp.replace('s','0')
+    rsp =rsp.replace('p','2')
+    return rsp
