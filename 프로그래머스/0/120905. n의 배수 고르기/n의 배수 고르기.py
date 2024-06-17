61709482841494037ee3a539c245c7de16c02173
손정우 <50861700+wonder21c@users.noreply.github.com>
2024-06-17 14:19:03 +0900

    Update n의 배수 고르기.py

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -4,3 +4,13 @@ def solution(n, numlist):
         if i % n == 0:
             answer.append(i)
     return answer
+
+
+
+def solution(n, numlist):
+    return list(filter(lambda v: v%n==0, numlist))
+
+
+def solution(n, numlist):
+    answer = [i for i in numlist if i%n==0]
+    return answer
