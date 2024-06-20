acd4a97501c18c83eb456768697b084d8b372df7
손정우 <50861700+wonder21c@users.noreply.github.com>
2024-06-20 19:07:13 +0900

    Update 정수를 나선형으로 배치하기.py

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,30 +1,27 @@
 def solution(n):
     if n == 1:
         return [[1]]
-    
-    answer = [[0 for j in range(n)] for i in range(n)] # 배열 초기화
-    
-    x = 0
-    y = 0
-    dir = 'r'
-    
+    answer = [[0 for i in range(n)] for i in range(n)]
+    a = 0
+    b = 0
+    start = "r"
     for i in range(n*n):
-        answer[x][y] = i + 1
-        if dir == 'r':
-            y += 1
-            if y == n-1 or answer[x][y+1] != 0: # 맨 끝에 도달했거나 가려는 곳에 이미 값이 있으면 방향 전환
-                dir = 'd'
-        elif dir == 'd':
-            x += 1
-            if x == n-1 or answer[x+1][y] != 0:
-                dir = 'l'
-        elif dir == 'l':
-            y -= 1
-            if y == 0 or answer[x][y-1] != 0:
-                dir = 'u'
-        elif dir == 'u':
-            x -= 1
-            if x == n-1 or answer[x-1][y] != 0:
-                dir = 'r'
-                
+        answer[a][b] = i + 1
+        if start == "r":
+            b += 1
+            if b == n - 1 or answer[a][b+1] != 0:
+                start = "d"
+        elif start == "d":
+            a += 1
+            if a == n - 1 or answer[a+1][b] != 0:
+                start = "l"
+        elif start == "l":
+            b -= 1
+            if b == 0 or answer[a][b-1] != 0:
+                start = "u"
+        elif start == "u":
+            a -= 1
+            if a == 0 or answer[a-1][b] != 0:
+                start = "r"       
+            
     return answer
