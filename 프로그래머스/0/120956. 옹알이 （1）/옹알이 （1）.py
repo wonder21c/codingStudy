747066bc1834b4d1ad1cffc6d300d835ae84a7f3
손정우 <50861700+wonder21c@users.noreply.github.com>
2024-06-20 17:58:44 +0900

    Update 옹알이 （1）.py

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -5,11 +5,19 @@ def solution(babbling):
     result = 0
     
     for word in babbling:
-        
         pattern = '^(' + '|'.join(patterns) + ')+$'
-        
-        
         if re.match(pattern, word):
             result += 1
     
     return result
+
+
+import re
+
+def solution(babbling):
+    regex = re.compile('^(aya|ye|woo|ma)+$')
+    cnt=0
+    for e in babbling:
+        if regex.match(e):
+            cnt+=1
+    return cnt
